0ba9349d54105d684f690d450dc2241c2f360766
udodka <ketypavlovsk@gmail.com>
2022-06-08 12:35:26 +0300

    getTime test

---
+++ src/main/java/com/example/musicplayer/HelloControllerTest.java
@@ -0,0 +1,84 @@
+package com.example.musicplayer;
+
+import javafx.util.Duration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloControllerTest {
+    //HelloController control;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Before HelloControllerTest.class");
+        //control = new HelloController();
+        //control.initialize(null, null);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("After HelloControllerTest.class");
+        //control = null;
+    }
+
+    @Test
+    public void initialize() {
+    }
+
+    @Test
+    public void selectItem() {
+    }
+
+    @Test
+    public void playMedia() {
+
+    }
+
+    @Test
+    public void getTime() throws Exception{
+        String result = HelloController.getTime(new Duration(342000.0));
+        assertEquals(result, "05:42");
+    }
+
+    @Test
+    public void pauseMedia() {
+    }
+
+    @Test
+    public void playPause() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void search() {
+    }
+
+    @Test
+    public void addPlaylist() {
+    }
+
+    @Test
+    public void importPlaylist() {
+    }
+
+    @Test
+    public void beginTimer() {
+    }
+
+    @Test
+    public void cancelTimer() {
+    }
+
+    @Test
+    public void cycleTrack() {
+    }
+}
